8dbda1150d985c1d0d27907358cef41fd3b689f9
Adebayo Akinlalu <aakinlalu@outlook.com>
2025-01-23 11:52:24 +1100

    Add Ollama structural output and function calling

---
+++ llm_using_Javascript/ollamaStructuralOutput.js
@@ -0,0 +1,21 @@
+import ollama from 'ollama';
+import { z} from 'zod';
+import { zodToJsonSchema } from 'zod-to-json-schema';
+
+const Country = z.object({
+    name: z.string(),
+    capital: z.string(),
+    languages: z.array(z.string())
+});
+
+const response = await ollama.chat({
+    model: 'llama3.1',
+    messages: [{
+        'role': 'user',
+        'content': 'Tell me about Australia'
+    }],
+    format: zodToJsonSchema(Country)
+})
+
+const country = Country.parse(JSON.parse(response.message.content));
+console.log(country);
